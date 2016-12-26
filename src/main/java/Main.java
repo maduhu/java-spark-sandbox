96329822970ede9fa06a2a1bf5eb7dd3959640e2
Alexander Solovyov <alexander.solovyov@team.wrike.com>
2016-12-27 02:05:00 +0300

    Added simple spark rdd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+
+import java.io.File;
 
 /**
  * @author alexander.solovyov
@@ -7,7 +10,16 @@ import org.apache.spark.api.java.JavaSparkContext;
  */
 public class Main {
     public static void main(String[] args) {
-        SparkConf conf = new SparkConf();
+        String logFile = System.getProperty("user.dir") + File.separator + "java-spark-sandbox.iml";
+
+        SparkConf conf = new SparkConf().setAppName("Simple Application").setMaster("local");
         JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> logData = sc.textFile(logFile).cache();
+
+        long numAs = logData.filter(s -> s.contains("a")).count();
+
+        long numBs = logData.filter(s -> s.contains("b")).count();
+
+        System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
     }
 }
