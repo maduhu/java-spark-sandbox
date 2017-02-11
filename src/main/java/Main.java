656fba488b5dbdfa4a6b6ae962865df2ea88a829
Alexander Solovyov <alexander.solovyov@team.wrike.com>
2017-02-11 20:35:31 +0300

    added gitignore, spring support

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.File;
 
@@ -9,16 +10,19 @@ import java.io.File;
  * @since 27.12.16
  */
 public class Main {
-    public static void main(String[] args) {
-        String logFile = System.getProperty("user.dir") + File.separator + "java-spark-sandbox.iml";
+    public static void main(final String[] args) {
 
-        SparkConf conf = new SparkConf().setAppName("Simple Application").setMaster("local");
-        JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaRDD<String> logData = sc.textFile(logFile).cache();
+        final ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:/META-INF/spring-config.xml");
 
-        long numAs = logData.filter(s -> s.contains("a")).count();
+        final String logFile = System.getProperty("user.dir") + File.separator + "java-spark-sandbox.iml";
 
-        long numBs = logData.filter(s -> s.contains("b")).count();
+        final SparkConf conf = new SparkConf().setAppName("Simple Application").setMaster("local");
+        final JavaSparkContext sc = new JavaSparkContext(conf);
+        final JavaRDD<String> logData = sc.textFile(logFile).cache();
+
+        final long numAs = logData.filter(s -> s.contains("a")).count();
+
+        final long numBs = logData.filter(s -> s.contains("b")).count();
 
         System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
     }
